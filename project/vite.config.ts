254a3546c649d9d227293d9516f772ae0aba48cf
Robert Rongen <robert.rongen@ziggo.nl>
2025-01-18 21:20:19 +0100

    update server base settings for production

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/arda/', // Base path for production deployment
   server: {
     proxy: {
       '/api': {
